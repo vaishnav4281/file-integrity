1f53c576d3e9e333849aac06b9195af40e92a3d5
VAISHNAV K <89403217+vaishnav4281@users.noreply.github.com>
2025-12-17 21:53:08 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 echo "Building the project..."
 python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+
+echo "Collecting static files..."
+# This will now use the new STATIC_ROOT (staticfiles_build)
+python3 manage.py collectstatic --noinput --clear
+
+echo "Build process completed."
