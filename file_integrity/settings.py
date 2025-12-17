2cb6f256a0b4f9c9217c01ae72b5f3cda209a4d6
VAISHNAV K <89403217+vaishnav4281@users.noreply.github.com>
2025-12-17 21:50:43 +0530

    Update settings.py

--- file_integrity/settings.py
+++ file_integrity/settings.py
@@ -111,12 +111,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+# Change this line
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' 
 
+# Keep these as they are
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
